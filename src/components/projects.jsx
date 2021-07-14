9f6e243d5b0814c6a2bcf5bac8d1e02e0d96029f
Xignotic <xignotic84@gmail.com>
2021-07-14 16:08:17 +0200

    Add Shiro to projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -10,7 +10,10 @@ export default function Projects() {
                     description="Managed a diverse online community of over 100, 000 members. I work on improving and enhancing users experience on the site by writing guidelines to protect our users and the site. I've developed multiple chat bots to help support the moderation of the community and site."/>
       < Projectcard url='http://dblstatistics.com' image='/dblstats' title='DBL Statistics' date="July 2020 - Present"
                     position='Full-Stack Developer'
-                    description='DBL Statistics is an analytical project that uses data from top.gg. This project was unfunded and was worked on by myself and two others. We decided to take on this project to provide developers and community members regular and consistent data regarding the growth and progress of the bots they use and create.'/>
+                    description='DBL Statistics is an analytical project that uses data from Top.gg. This project was unfunded and was worked on by myself and two others. We decided to take on this project to provide developers and community members regular and consistent data regarding the growth and progress of the bots they use and create.'/>
+      < Projectcard url='https://shiro.gg' image='/shiro' title='Shiro' position='Founder and Developer'
+                    date="September 2018 - Current"
+                    description='I am the founder and currently a developer for Shiro. Shiro is a multi-functional project that provides an public API and a Discord Bot.'/>
     </div>
   )
 }
