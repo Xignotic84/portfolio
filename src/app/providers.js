758ac83619eadeee6b3f16783d581dbb7c667a6a
Franz Herrmann <Xignotic84@gmail.com>
2024-06-12 16:37:03 +0200

    Add google analytics

--- src/app/providers.js
+++ src/app/providers.js
@@ -3,6 +3,8 @@
 import { CacheProvider } from '@chakra-ui/next-js'
 import {ChakraProvider, extendTheme} from '@chakra-ui/react'
 import {theme} from "../../theme/theme";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export function Providers({ children }) {
   return (
@@ -10,6 +12,7 @@ export function Providers({ children }) {
         <ChakraProvider theme={extendTheme(theme)}>
           {children}
         </ChakraProvider>
+        <GoogleAnalytics gaId="G-1HPYCRV7ND" />
       </CacheProvider>
   )
 }
